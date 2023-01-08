de3f17a3d248761a449d495eba76b84e9ab0ae78
rados1k <radik.052005@gmail.com>
2023-01-08 14:11:08 +0100

    8ene

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,7 @@ import { CryptoCurrencies, News } from '../components';
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery(10);
+  const { data, isFetching } = useGetCryptosQuery(12);
   const globalStats = data?.data?.stats
   
   if(isFetching) return 'Loading...';
@@ -25,7 +25,7 @@ const Homepage = () => {
         <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)}/></Col>
       </Row>
       <div className='home-heading-container'>
-        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
+        <Title level={2} className='home-title'>Top Cryptocurrencies in the world</Title>
         <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
       </div>
       <CryptoCurrencies simplified/>
